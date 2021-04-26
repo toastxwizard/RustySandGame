630f27df9328745c55f43b6322cf2da85e2b3434
Toastwizard <djlegierse@hotmail.com>
2021-04-26 20:17:23 +0200

    Working Sand

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use materials::{matter::Matter};
 
 use piston_window::*;
 
+const X_LIMIT : usize = 640;
+const Y_LIMIT : usize = 480;
+
 fn main() {
     let mut window: PistonWindow = WindowSettings::new("Hello Piston!", (640, 480))
         .exit_on_esc(true)
@@ -17,7 +20,7 @@ fn main() {
 
     let mut board : Vec<Option<Box<dyn Matter>>> = vec![];
 
-    for _ in 0..(640 * 480){
+    for _ in 0..(X_LIMIT * Y_LIMIT){
         board.push(None);
     }
 
@@ -28,70 +31,66 @@ fn main() {
 
     while let Some(e) = window.next() {
 
-
         if let Some(args) = e.mouse_cursor_args(){
             mouse_position[0] = (args[0].round()) as usize;
             mouse_position[1] = (args[1].round()) as usize;
-        }
-
-        if let Some(Button::Mouse(MouseButton::Left)) = e.press_args(){
-            let boxed_matter : Box<dyn Matter> = Box::new(Sand::new(25.0));
-
-            board[mouse_position[0] + (mouse_position[1] * 640)] = Some(boxed_matter);
-        }
-
-        if let Some(Button::Mouse(MouseButton::Left)) = e.press_args(){
-            is_pressed = true;
         }
 
         if let Some(Button::Mouse(MouseButton::Left)) = e.release_args(){
             is_pressed = false;
         }
 
-        if(is_pressed){
-            let boxed_matter : Box<dyn Matter> = Box::new(Sand::new(25.0));
-
-            board[mouse_position[0] + (mouse_position[1] * 640)] = Some(boxed_matter);
+        if let Some(Button::Mouse(MouseButton::Left)) = e.press_args(){
+            is_pressed = true;
         }
 
+        if is_pressed {
+            let boxed_matter : Box<dyn Matter> = Box::new(Sand::new(25.0));
+
+            board[mouse_position[0] + (mouse_position[1] * X_LIMIT)] = Some(boxed_matter);
+        }
 
         if let Some(_args) = e.render_args(){
             window.draw_2d(&e, |c, g, _d| {
                 clear([0.0, 0.0, 0.0, 0.0], g);
 
-                for n in 0..(640 * 480) {
+                for n in 0..(X_LIMIT * Y_LIMIT) {
                     if let Some(matter) =  &board[n]{
-                        let x = n % 640;
-                        let y = n / 640;
-                        rectangle(matter.get_color(), [x as f64, y as f64, 2f64, 2f64], c.transform, g);
+                        let x = n % X_LIMIT;
+                        let y = n / X_LIMIT;
+                        rectangle(matter.get_color(), [x as f64, y as f64, 1f64, 1f64], c.transform, g);
                     }
                   
                 }
-
             });
         }
 
-        if let Some(_args) = e.update_args(){
-            for i in 0usize..(640 * 479){
-                if let Some(matter) = &board[i]{
-                    if *(&board[i + 640].is_none()){
-                        board[i] = None;
-                        board[i + 640] = Some(Box::new(Sand::new(25f32)));
-                    }
+        if let Some(_) = e.update_args(){
+            let mut coord_stack : Vec<(usize, usize)> = vec![];
 
-                    if let Some(_) = &board[i + 640]{
-                        if *(&board[i + 639].is_none()){
-                            board[i] = None;
-                            board[i + 639] = Some(Box::new(Sand::new(25f32)));
-                        }
-                        
-                        // if *(&board[i + 641].is_none()){
-                        //     board[i] = None;
-                        //     board[i + 641] = Some(Box::new(Sand::new(25f32)));
-                        // }
-                    }
+           for n in 0..(X_LIMIT * (Y_LIMIT - 1)) {
+                if let Some(_) = &board[n] {
+                    let x = n % X_LIMIT;
+                    let y = n / X_LIMIT;
+                    &coord_stack.push((x,y));
+                }             
+           }
+
+           for n in coord_stack {
+                let index = n.0 + (n.1 * X_LIMIT);
+                let below = index + X_LIMIT;
+
+                if *(&board[below].is_none()){
+                    board[index] = None;
+                    board[below] = Some(Box::new(Sand::new(25f32)));
+                }else if *(&board[below - 1].is_none()) && ((below - 1) % X_LIMIT) > 0{
+                    board[index] = None;
+                    board[below - 1] = Some(Box::new(Sand::new(25f32)));
+                }else if *(&board[below + 1].is_none()) && ((below + 1) % X_LIMIT) < (X_LIMIT - 1){
+                    board[index] = None;
+                    board[below + 1] = Some(Box::new(Sand::new(25f32)));
                 }
-            }
+           }
         }
     }
 }
